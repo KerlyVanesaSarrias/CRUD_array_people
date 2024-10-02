e2192a0119e91f122fe8f9ad34e206a8e1174e60
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-10-01 19:52:43 -0500

    The props of People were passed to Person

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import PropTypes from "prop-types";
+import { Person } from "./Person";
 
 export const People = ({ people, setPeople }) => {
     return (
         <div>
             <h2 className='text-center my-4'>IT Team</h2>
-            <div className='container '></div>
+            <div className='container'>
+                <div className='row d-flex flex-wrap row-cols-1 row-cols-md-2 row-cols-lg-3'>
+                    {
+                        people.map((people) => {
+                            return (
+                                <div key={people.id}>
+                                    <Person
+                                        id={people.id}
+                                        name={people.name}
+                                        img={people.img}
+                                        role={people.role}
+                                    />
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 };
@@ -13,5 +31,3 @@ People.propTypes = {
     people: PropTypes.array,
     setPeople: PropTypes.func
 }
-
-
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,16 +1,26 @@
+import PropTypes from "prop-types";
 
-export const Person = () => {
+export const Person = ({ id, name, role, img }) => {
     return (
         <div className='col'>
             <div className="card" style={{ width: "18rem;" }}>
-                <img src="https://bootdey.com/img/Content/avatar/avatar3.png" className="card-img-top" alt="avatar1" />
+                <img src={img} className="card-img-top" alt={name} />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">role</p>
+                    <h3 className="card-title">{name}</h3>
+                    <p className="card-text">{role}</p>
+                </div>
+                <div className='container mb-4'>
                     <button className="btn btn-success mx-2">Editar</button>
                     <button className="btn btn-danger">Eliminar</button>
                 </div>
             </div>
         </div>
     )
+};
+
+Person.propTypes = {
+    id: PropTypes.number,
+    name: PropTypes.string,
+    role: PropTypes.string,
+    img: PropTypes.string
 }
