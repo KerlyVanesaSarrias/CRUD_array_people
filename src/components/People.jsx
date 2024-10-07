93f437056f04f039beed4c17105fdb27fa5e8841
Kerly Sarrias <kerlysarrias011@gmail.com>
2024-10-07 16:03:55 -0500

    added handlechanged and create functions

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,7 +1,35 @@
 import PropTypes from "prop-types";
 import { Person } from "./Person";
+import { useState } from "react";
 
 export const People = ({ people, setPeople }) => {
+
+const [editingId, setEditingId] = useState(people);
+const [editedPerson, setEditedPerson] = useState(
+    {
+        name: '',
+        role: '',
+        img:''
+    });
+
+const handleChange =(e) => {
+    const { name, value } = e.target;
+    setEditedPerson((prev) => ({ ...prev, [name]: value }));
+}
+
+const handleCreate = (e) => {
+    e.preventDefault();
+    setPeople([...people,{id:people.length + 1, ...editedPerson}])
+    setEditedPerson({
+        name: '',
+        role: '',
+        img:''
+    })
+}
+
+
+
+
     return (
         <div>
             <h2 className='text-center my-4'>IT Team</h2>
@@ -22,6 +50,9 @@ export const People = ({ people, setPeople }) => {
                         })
                     }
                 </div>
+            </div>
+            <div>
+                
             </div>
         </div>
     )
